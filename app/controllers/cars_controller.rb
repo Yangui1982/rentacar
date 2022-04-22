83ecc707c2f67403ce4ccb4a7c70d67e1ce87717
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-04-22 16:35:49 +0200

    tables corrections

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,4 +7,16 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
     @booking = Booking.new
   end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    redirect_to car_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :photo, :price)
+  end
 end
