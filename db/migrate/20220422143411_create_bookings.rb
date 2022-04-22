bf9780b0310f0cea2b39df744314fc91ab5f4dad
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-04-22 16:37:08 +0200

    some ugs fixed

--- db/migrate/20220422143411_create_bookings.rb
+++ db/migrate/20220422143411_create_bookings.rb
@@ -4,7 +4,7 @@ class CreateBookings < ActiveRecord::Migration[6.1]
       t.date :start_date
       t.date :end_date
       t.references :user, null: false, foreign_key: true
-      t.references :castle, null: false, foreign_key: true
+      t.references :car, null: false, foreign_key: true
 
       t.timestamps
     end
